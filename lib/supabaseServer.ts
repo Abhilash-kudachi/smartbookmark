a50e2a76bdf291549e70eaaddd3b1441e933054f
Abhilash Kudachi <abhilashkudachi2002@gmail.com>
2026-02-18 16:18:23 +0530

    fix: async cookies for supabase server client

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,9 +1,27 @@
 import { cookies } from 'next/headers'
 import { createServerClient } from '@supabase/ssr'
 
-export const createSupabaseServer = () =>
-  createServerClient(
+export const createSupabaseServer = async () => {
+  const cookieStore = await cookies() 
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // This can be ignored if the client is called from a Server Component
+          }
+        },
+      },
+    }
   )
+}
